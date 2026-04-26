b25a4c0e6f500410fb5d779fd17c18f49cd248ae
Samyak Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-04-26 14:53:23 +0530

    Update WearherCard.js

--- src/components/WearherCard.js
+++ src/components/WearherCard.js
@@ -1,1 +1,19 @@
 
+
+const WeatherCard = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p>🌡 Temp: {data.main.temp}°C</p>
+      <p>💧 Humidity: {data.main.humidity}%</p>
+      <p>☁ Condition: {data.weather[0].main}</p>
+
+      <img
+        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        alt="weather icon"
+      />
+    </div>
+  );
+};
+
+export default WeatherCard;
