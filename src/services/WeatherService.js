9bbc4e0e9c35150a68bd3fecaf342876a366f409
Samyak Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-04-26 14:53:46 +0530

    Update WeatherService.js

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,1 +1,15 @@
 
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+export const fetchWeather = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+
+  if (!response.ok) {
+    throw new Error("City not found");
+  }
+
+  return await response.json();
+};
