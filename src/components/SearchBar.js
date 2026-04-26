00679207967dd3cc9e239211030763c626bfc05c
Samyak Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2026-04-26 14:53:04 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,26 @@
 
+import { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(city);
+    setCity("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
